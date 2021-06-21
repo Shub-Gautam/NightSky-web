c004d2651e5981f58f481715ad0a613f64a2ea8e
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2021-06-21 21:33:31 +0530

    requirements

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ const https = require('https');
 
 const app = express();
 
-
 app.get("/",(req,res)=>{
     https.get("https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=c78ba97012540bcc813d1e5e9025fbde",()=>{
             console.log(response);
@@ -11,8 +10,11 @@ app.get("/",(req,res)=>{
     res.send("Server is up and running");
 });
 
-
+app.post("/post",function(req,res){
+    var body1 = req.body;
+    console.log(body1);
+});
 
 app.listen(8080,()=>{
-    console.log("Listning to port 8081");
+    console.log("Listning to port 8080");
 });
